d6bd831f769b7713eba15fe78f3dca30af211a01
NehaKale1403 <nkale8013@gmail.com>
2020-10-15 23:16:16 +0530

    added comments

--- Python_Basics/runnerup_score.py
+++ Python_Basics/runnerup_score.py
@@ -2,6 +2,7 @@ if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
 
+    #here we will sort the list and then find the second last score
     runnerup = sorted(list(set(arr)))[-2]
 
     print(runnerup)
