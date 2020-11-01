edf5b809a737a9ec787690fd7d01c6df9e8cdd6b
NehaKale1403 <nkale8013@gmail.com>
2020-11-01 23:09:58 +0530

    sum of prime factors of a number

--- DataStructures_and_Algorithms/sum_of_prime_factors_of_n.cpp
+++ DataStructures_and_Algorithms/sum_of_prime_factors_of_n.cpp
@@ -1,1 +1,38 @@
-//to find tthe sum of all the prime factors from a given number
+// to find the sum of all the prime factors of a given number
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n){
+
+	if(n <= 1)
+		return false;
+	if(n == 2)
+		return true;
+
+	for(int i = 3; i < n; i++){
+
+		if(n%i == 0){
+			return false;
+		}
+	}
+	return true;
+}
+
+int main(){
+
+	int n;
+	cin>>n;
+	int sum = 0;
+
+	for(int i=2; i<=n ; i++){
+	    
+	    if(n%i==0){
+	        
+    	   if(isPrime(i)){
+    	
+    			sum += i;
+    		}
+	    }
+	}
+	cout<<sum;
+}
