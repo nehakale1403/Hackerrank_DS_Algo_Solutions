7fb378f67ec0082d29418f870cf5c416405655d2
Neha Kale <nkale8013@gmail.com>
2020-12-02 12:32:01 +0530

    Printed the top view elements of a tree

---
+++ DataStructures_and_Algorithms/top_view_of_a_tree.cpp
@@ -0,0 +1,91 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+                }
+
+               return root;
+           }
+        }
+
+/*
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+    void topView(Node * root) {
+        
+        static Node *temp = root;
+        
+        if(root==NULL)
+            return;
+        
+        topView(root->left);
+        cout<<root->data<<" ";
+        if(root == temp){
+            root=root->right;//not printing the root element twice
+            
+            while(root != NULL){
+                cout<<root->data<<" ";
+                root=root->right;
+            }
+            
+        }
+    }
+
+}; //End of Solution
+
+int main() {
+    
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+    myTree.topView(root);
+
+    return 0;
+}
